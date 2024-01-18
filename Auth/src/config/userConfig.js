7155f2da2b00979fa23d7d52101c83914c5201ae
Joan Kabura <kaburajoan25@gmail.com>
2024-01-18 19:15:20 +0300

    feat: Database connection error rectified9

--- Auth/src/config/userConfig.js
+++ Auth/src/config/userConfig.js
@@ -12,8 +12,8 @@ const Config = {
         idleTimeoutMillis: 30000
     },
     options: {
-        encrypt: true, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
+        encrypt: true, 
+        trustServerCertificate: false 
     }
     }
     
--- Auth/src/validators/userRegistrationValidator.js
+++ Auth/src/validators/userRegistrationValidator.js
@@ -2,10 +2,11 @@ const Joi = require('joi')
 
 const userSchema = Joi.object({
     FirstName: Joi.string().required(),
-	LastName: Joi.string().required(),
-	UserEmail: Joi.string().email({ minDomainSegments: 2, tlds: {allow: ['com', 'net']}}).required(),
-    UserPasswordHash: Joi.string().pattern(new RegExp("^[A-Za-z0-9]")).required(),
-    UserCPassword: Joi.ref('UserPasswordHash'),
-}).with('UserPasswordHash', 'UserCPassword')
+    LastName: Joi.string().required(),
+    UserEmail: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    UserPasswordHash: Joi.string().min(6).required(), // Adjust the minimum length as needed
+    UserCPassword: Joi.string().valid(Joi.ref('UserPasswordHash')).required(),
+}).with('UserPasswordHash', 'UserCPassword');
+
 
 module.exports = userSchema;
