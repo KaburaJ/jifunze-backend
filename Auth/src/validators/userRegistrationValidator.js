4acb63698ce01c7c613fa0a4ae6b4dc7ca68fb60
Joan Kabura <kaburajoan25@gmail.com>
2024-01-18 19:35:02 +0300

    feat: Database connection error rectified11

--- Auth/src/validators/userRegistrationValidator.js
+++ Auth/src/validators/userRegistrationValidator.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
     FirstName: Joi.string().required(),
     LastName: Joi.string().required(),
     UserEmail: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    UserPasswordHash: Joi.string().min(6).required(), // Adjust the minimum length as needed
+    UserPasswordHash: Joi.string().min(6).required(), 
     UserCPassword: Joi.string().valid(Joi.ref('UserPasswordHash')).required(),
 }).with('UserPasswordHash', 'UserCPassword');
 
