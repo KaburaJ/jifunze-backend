23204470a759d90bdddf88fa1d62f3bae7fa2a4a
Joan Kabura <kaburajoan25@gmail.com>
2024-01-21 08:04:04 +0300

    feat: SwaggerUI docs added3

--- Auth/src/routers/userRoutes.js
+++ Auth/src/routers/userRoutes.js
@@ -1,3 +1,87 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         FirstName:
+ *           type: string
+ *         LastName:
+ *           type: string
+ *         UserEmail:
+ *           type: string
+ *         UserPasswordHash:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: User authentication and authorization
+ */
+
+/**
+ * @swagger
+ * /user/signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User registration successful
+ *       400:
+ *         description: Bad request or validation error
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User login successful
+ *       400:
+ *         description: Bad request or validation error
+ *       401:
+ *         description: Incorrect password
+ *       404:
+ *         description: No user found
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /user/logout:
+ *   get:
+ *     summary: Logout a user
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: User logout successful
+ *       500:
+ *         description: Internal server error
+ */
+
+
 const express = require('express')
 const { registerUser, loginUser, logoutUser } = require('../controllers/authController')
 const userRoutes = express.Router()
