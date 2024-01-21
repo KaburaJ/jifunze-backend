87f6aaa8266164948f53cb2fdd245b14e89af789
Joan Kabura <kaburajoan25@gmail.com>
2024-01-21 08:19:48 +0300

    feat: SwaggerUI docs added4

--- Auth/src/routers/userRoutes.js
+++ Auth/src/routers/userRoutes.js
@@ -1,5 +1,12 @@
 /**
  * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: User authentication and authorization
+ */
+
+/**
+ * @swagger
  * components:
  *   schemas:
  *     User:
@@ -17,9 +24,17 @@
 
 /**
  * @swagger
- * tags:
- *   name: Authentication
- *   description: User authentication and authorization
+ * components:
+ *   schemas:
+ *     LoginRequest:
+ *       type: object
+ *       properties:
+ *         UserEmail:
+ *           type: string
+ *           format: email
+ *         UserPasswordHash:
+ *           type: string
+ *           pattern: "^[A-Za-z0-9]"
  */
 
 /**
@@ -54,16 +69,14 @@
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/User'
+ *             $ref: '#/components/schemas/LoginRequest'
  *     responses:
  *       200:
  *         description: User login successful
  *       400:
  *         description: Bad request or validation error
  *       401:
- *         description: Incorrect password
- *       404:
- *         description: No user found
+ *         description: Incorrect password or user not found
  *       500:
  *         description: Internal server error
  */
@@ -81,13 +94,12 @@
  *         description: Internal server error
  */
 
+const express = require('express');
+const { registerUser, loginUser, logoutUser } = require('../controllers/authController');
+const userRoutes = express.Router();
 
-const express = require('express')
-const { registerUser, loginUser, logoutUser } = require('../controllers/authController')
-const userRoutes = express.Router()
+userRoutes.post('/user/signup', registerUser);
+userRoutes.post('/user/login', loginUser);
+userRoutes.get('/user/logout', logoutUser);
 
-userRoutes.post('/user/signup', registerUser)
-userRoutes.post('/user/login', loginUser)
-userRoutes.get('/user/logout', logoutUser)
-
-module.exports = userRoutes
+module.exports = userRoutes;
