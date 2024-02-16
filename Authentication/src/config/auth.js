f1df70e731de8cf9ce5e3c32a9d7ce2629e2ef08
Joan Kabura <99409674+KaburaJ@users.noreply.github.com>
2024-02-17 02:07:52 +0300

    Update auth.js

--- Authentication/src/config/auth.js
+++ Authentication/src/config/auth.js
@@ -1,22 +1,26 @@
 const passport = require("passport");
 require("dotenv").config();
-var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID:     process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    return done(null, profile)
-  }
-));
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/google/callback",
+  passReqToCallback: true
+}, function(request, accessToken, refreshToken, profile, done) {
+  const user = {
+    FirstName: profile.name.givenName,
+    LastName: profile.name.familyName,
+    Email: profile.email
+  };
+
+  return done(null, user);
+}));
 
 passport.serializeUser(function(user, done) {
-  done(null, user)
-})
+  done(null, user);
+});
 
 passport.deserializeUser(function(user, done) {
-  done(null, user)
-})
+  done(null, user);
+});
