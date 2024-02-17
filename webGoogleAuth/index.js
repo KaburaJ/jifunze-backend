40040a89f586f6bb25dfbfb252b1e860e2ece211
Joan Kabura <kaburajoan25@gmail.com>
2024-02-17 07:36:17 +0300

    feat: added google auth and db integration12

---
+++ webGoogleAuth/index.js
@@ -0,0 +1,44 @@
+require("./src/config/auth");
+require("dotenv").config();
+const GoogleAuthRoutes= require("./src/routes/GoogleAuthRoutes");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: "https://jifunze-hub-google-signup.onrender.com/",
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);app.use(session({ secret: process.env.SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Swagger configuration
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Your API Documentation",
+      version: "1.0.0",
+      description: "Documentation for your API",
+    },
+  },
+  apis: ["./src/routes/*.js"], // Specify the path to your route files
+};
+const specs = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.get("/", (req, res) => {
+  res.send("Jifunze Hub");
+});
+app.use('/', GoogleAuthRoutes)
+
+
+
+app.listen(5000, () => console.log("App listening on port 5000"));
---
+++ webGoogleAuth/src/routes/GoogleAuthRoutes.js
@@ -0,0 +1,112 @@
+/**
+ * @swagger
+ * tags:
+ *   name: GoogleAuth
+ *   description: https://jifunze-hub-google-signup.onrender.com
+ */
+
+/**
+ * @swagger
+ * /auth/google:
+ *   get:
+ *     summary: Initiates Google OAuth authentication
+ *     description: Redirects to Google OAuth for authentication
+ *     responses:
+ *       200:
+ *         description: Successfully initiated OAuth
+ */
+
+/**
+ * @swagger
+ * /google/callback:
+ *   get:
+ *     summary: Handles Google OAuth callback
+ *     description: Handles the callback from Google OAuth authentication
+ *     responses:
+ *       200:
+ *         description: Successfully authenticated
+ *       401:
+ *         description: Authentication failed
+ */
+
+/**
+ * @swagger
+ * /auth/failure:
+ *   get:
+ *     summary: Authentication failure
+ *     description: Displays a message for authentication failure
+ *     responses:
+ *       200:
+ *         description: Authentication failed
+ */
+/**
+ * @swagger
+ * /protected:
+ *   get:
+ *     summary: Protected route
+ *     description: Displays a message for authenticated users
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully authenticated
+ *       401:
+ *         description: Authentication failed
+ */
+
+require('../config/auth.js')
+const express = require('express');
+const GoogleAuthRoutes = express.Router();
+const passport = require("passport");
+const mssql = require('mssql');
+const config = require('../config/dbConfig.js')
+
+GoogleAuthRoutes.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+GoogleAuthRoutes.post("/google/callback", async (req, res, next) => {
+  passport.authenticate("google", async (err, user) => {
+    if (err || !user) {
+      res.status(401).json({ success: false, message: 'Authentication failed' });
+      return;
+    }
+
+    try {
+      // Connect to the database
+      const sql = await mssql.connect(config);
+
+      // Call the stored procedure to add the user to the database
+      const request = new mssql.Request(sql);
+      request.input('FirstName', user.FirstName);
+      request.input('LastName', user.LastName);
+      request.input('UserEmail', user.Email);
+      request.input('UserPasswordHash', null);
+      const result = await request.execute('[dbo].[AddUser]');
+
+      if (result && result.rowsAffected && result.rowsAffected[0] > 0) {
+        res.status(200).json({ success: true, message: 'User added successfully', user: user });
+      } else {
+        res.status(500).json({ success: false, message: 'Failed to add user to the database' });
+      }
+    } catch (error) {
+      console.error('Error adding user to the database:', error);
+      res.status(500).json({ success: false, message: 'An error occurred while adding user to the database' });
+    }
+  })(req, res, next);
+});
+
+GoogleAuthRoutes.get("/auth/failure", (req, res) => {
+  res.send("Something went wrong on our end");
+});
+
+const isLoggedIn = (req, res, next) => {
+  req.user ? next() : res.sendStatus(401);
+};
+
+GoogleAuthRoutes.get("/protected", isLoggedIn, (req, res) => {
+  res.send("Hello!");
+});
+
+module.exports = GoogleAuthRoutes;
