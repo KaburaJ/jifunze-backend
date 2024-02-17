6dfdb5bc605755b20a0afbffeace64f4580d70de
Joan Kabura <kaburajoan25@gmail.com>
2024-02-17 11:56:09 +0300

    feat: added google auth and db integration 2

--- Authentication/src/config/auth.js
+++ Authentication/src/config/auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  // callbackURL: "https://jifunze-hub-google-signup.onrender.com/google/callback",
+  callbackURL: '',
   passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
   const user = {
