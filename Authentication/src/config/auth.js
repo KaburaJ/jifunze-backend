29b3e77243f301ea34c35ab9f1d699048f2eac08
Joan Kabura <kaburajoan25@gmail.com>
2024-02-17 03:49:17 +0300

    feat: added google auth and db integration6

--- Authentication/src/config/auth.js
+++ Authentication/src/config/auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "http://localhost:5000/google/callback",
   passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
   const user = {
