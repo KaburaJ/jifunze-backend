d7c33943451e4cba433e40b101f54ffddad0e319
Joan Kabura <kaburajoan25@gmail.com>
2024-02-17 09:12:40 +0300

    feat: added google auth and db integration 11

--- Auth/src/controllers/authController.js
+++ Auth/src/controllers/authController.js
@@ -134,7 +134,7 @@ module.exports = {
         res.json({
           success: true,
           message: 'User added successfully',
-          data: results[0].message
+          data: results[0]
       });
 
         console.log('CONNECTED AT SIGN UP');
