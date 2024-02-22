51102b1be9d1c9a5e7196adaf12d4d329dee546b
Joan Kabura <kaburajoan25@gmail.com>
2024-02-22 17:36:17 +0300

    feat: solving google registration of new user error2

--- webGoogleAuth/index.js
+++ webGoogleAuth/index.js
@@ -1,60 +1,44 @@
+require("../webGoogleAuth/src/config/auth");
 require("dotenv").config();
+const GoogleAuthRoutes= require("./src/routes/GoogleAuthRoutes");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const mssql = require('mssql');
-const config = require('../webGoogleAuth/src/config/dbConfig')
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const cors = require("cors");
 const app = express();
-
-// Session configuration
-app.use(session({ 
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
-
-// Passport initialization
+app.use(
+  cors({
+    origin: "https://jifunze-hub-google-signup.onrender.com/",
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);app.use(session({ secret: process.env.SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Google OAuth strategy
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/google/callback",
-    passReqToCallback: true
-}, function(request, accessToken, refreshToken, profile, done) {
-    const user = {
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        email: profile.email 
-    };
+// Swagger configuration
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Your API Documentation",
+      version: "1.0.0",
+      description: "Documentation for your API",
+    },
+  },
+  apis: ["./src/routes/*.js"], // Specify the path to your route files
+};
+const specs = swaggerJsdoc(options);
 
-    return done(null, user);
-}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
+app.get("/", (req, res) => {
+  res.send("Jifunze Hub");
 });
+app.use('/', GoogleAuthRoutes)
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
 
-// Routes
-const GoogleAuthRoutes = require("./src/routes/GoogleAuthRoutes");
-app.use('/', GoogleAuthRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(5000, () => console.log("App listening on port 5000"));
--- webGoogleAuth/src/config/auth.js
+++ webGoogleAuth/src/config/auth.js
@@ -5,13 +5,13 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/google/callback",
+    callbackURL: "https://jifunze-hub-google-signup.onrender.com/google/callback",
     passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
     const user = {
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        email: profile.email 
+        FirstName: profile.name.givenName,
+        LastName: profile.name.familyName,
+        Email: profile.email 
     };
 
     return done(null, user);
