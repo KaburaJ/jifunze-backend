4250e0d9c6d6808ee8870897544a14347ac6fd38
Joan Kabura <kaburajoan25@gmail.com>
2024-02-22 17:01:02 +0300

    feat: solving google registration of new user error

--- webGoogleAuth/src/config/auth.js
+++ webGoogleAuth/src/config/auth.js
@@ -3,24 +3,24 @@ require("dotenv").config();
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://jifunze-hub-google-signup.onrender.com/google/callback",
-  passReqToCallback: true
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://jifunze-hub-google-signup.onrender.com/google/callback",
+    passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
-  const user = {
-    FirstName: profile.name.givenName,
-    LastName: profile.name.familyName,
-    Email: profile.email
-  };
+    const user = {
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.email 
+    };
 
-  return done(null, user);
+    return done(null, user);
 }));
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
